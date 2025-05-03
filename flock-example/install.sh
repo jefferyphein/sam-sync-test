f49fada220797ab4355afe71414630251e13b48b
Jeffery Hein <jeffery.p.hein@gmail.com>
2025-05-03 11:03:47 -0400

    minor updates to install.sh

--- flock-example/install.sh
+++ flock-example/install.sh
@@ -2,8 +2,9 @@
 
 # Arguments to this script are as follows:
 #
-#   $1 -> the artifacts directory (i.e. the build directory for the lambda)
-#   $2 -> an argument indicating to run the protected locked command
+#   $1 -> the artifacts directory (i.e. the build directory for the lambda).
+#   $2 -> an argument indicating to run the protected locked command. Any value
+#         will suffice, as long as it is non-empty.
 #
 # We assume that this directory contains a file named "requirements.txt"
 #
@@ -23,19 +24,25 @@ mkdir -p "$install_dir"
 
 # This block is run outside of the lock.
 if [ -z "$2" ]; then
+  # Block until the lock is acquired and the protected code returns.
   flock "$lock_file" "$0" "$1" "run_with_lock"
+
+  # Once the lock is released, copy the installed artifacts only if certain
+  # conditions are met.
   if [ -e "$install_dir/.INSTALLED" ] && [ ! -e "$install_dir/.FAILED" ]; then
     cp -r "$install_dir/." "$1"
   fi
 
 # This block is run within the lock.
 else
-  # Only run "pip install" if the .INSTALLED file doesn't exist.
-  # NOTE: We cannot use the lock file here, because it will be created by "flock".
+  # If the .FAILED file exists, fail immediately.
   if [ -e "$install_dir/.FAILED" ]; then
     echo "Install failed, refusing to proceed."
     exit 1
-  elif [ ! -e "$install_dir/.INSTALLED" ]; then
+
+  # Only run "pip install" if the .INSTALLED file doesn't exist.
+  # NOTE: We cannot use the lock file here, because it will be created by "flock".
+  if [ ! -e "$install_dir/.INSTALLED" ]; then
     pip install -r "$requirements" -t "$install_dir" >> "$log_file" 2>&1
     rc=$?
     if [ ! -n $rc ]; then
