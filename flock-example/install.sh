68e43105336d90a50d9eaa7538ef72d1b49e41b4
Jeffery Hein <jeffery.p.hein@gmail.com>
2025-05-04 09:53:00 -0400

    updated flock-example install logic

--- flock-example/install.sh
+++ flock-example/install.sh
@@ -9,34 +9,46 @@
 
 set -euo pipefail
 
-if ! command -v flock >/dev/null; then
-  echo "Cannot find 'flock' command."
-  exit 1
-fi
-
+# Input arguments.
 artifacts_dir="$1"
-requirements="${artifacts_dir}/requirements.txt"
+run_with_lock="${2:-}"
+
+function pip_install() {
+  requirements="$1"
+  install_dir="$2"
+  log_file="$3"
+  pip install -r "$requirements" -t "$install_dir" >> "$log_file" 2>&1 
+}
 
 # Assume that the artifacts directory contains "requirements.txt"
+requirements="${artifacts_dir}/requirements.txt"
 if [ ! -e "$requirements" ]; then
   echo "Artifacts directory must contain 'requirements.txt' file."
   exit 1
 fi
 
-# Calculate the SHA1 hash of the requirements.txt file.
-requirements_sha1=$(sha1sum "$requirements" | awk '{print $1}')
-
 # Find the .aws-sam/ root directory.
 aws_sam_dir="${artifacts_dir%/.aws-sam/*}/.aws-sam"
 
+# If the "flock" command doesn't exist, simply "pip install" into artifacts directory.
+if ! command -v flock >/dev/null; then
+  log_file="${artifacts_dir%/}.log"
+  pip_install "$requirements" "$artifacts_dir" "$log_file"
+  rc=$?
+  exit $rc
+fi
+
+# Calculate the SHA1 hash of the requirements.txt file.
+requirements_sha1=$(sha1sum "$requirements" | awk '{print $1}')
+
 # Determine the paths we need and create directories, if necessary.
-install_dir="$aws_sam_dir/pip-install/$requirements_sha1"
-log_file="$aws_sam_dir/pip-install/$requirements_sha1.log"
+install_dir="$aws_sam_dir/build/pip-install/$requirements_sha1"
+log_file="$aws_sam_dir/build/pip-install/$requirements_sha1.log"
 lock_file="$install_dir/.aws-sam.lock"
 mkdir -p "$install_dir"
 
 # This block is run outside of the lock.
-if [ $# -gt 1 ] && [ -z "$2" ]; then
+if [ -z "$run_with_lock" ]; then
   # Block until the lock is acquired and the protected code returns.
   flock "$lock_file" "$0" "$artifacts_dir" "run_with_lock"
 
@@ -48,6 +60,8 @@ if [ $# -gt 1 ] && [ -z "$2" ]; then
 
 # This block is run within the lock.
 else
+  # NOTE: All commands within this block are protected under the file lock.
+
   # If the .FAILED file exists, fail immediately.
   if [ -e "$install_dir/.FAILED" ]; then
     echo "Install failed, refusing to proceed."
@@ -55,7 +69,6 @@ else
   fi
 
   # Only run "pip install" if the .INSTALLED file doesn't exist.
-  # NOTE: We cannot use the lock file here, because it will be created by "flock".
   if [ ! -e "$install_dir/.INSTALLED" ]; then
     pip install -r "$requirements" -t "$install_dir" >> "$log_file" 2>&1
     rc=$?
@@ -65,6 +78,12 @@ else
       touch "$install_dir/.FAILED"
       exit 1
     fi
+    # Remove installation artifacts.
+    find "$install_dir" -name "*.pyc" -type f -delete
+    find "$install_dir" -name __pycache__ -type d -delete
+    # Drop a file indicating successful installation.
     touch "$install_dir/.INSTALLED"
   fi
+
+  # NOTE: The file lock will be released immediately after this script terminates.
 fi
